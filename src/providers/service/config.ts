cd69b5e15e22a6c19217ae0032a0ccda5ff241e9
We Are Local <app@keeplocal.info>
2018-08-15 20:09:41 +0100

    Penguin is now a fully hybrid app.
    Version 1.0 just opened punkypenguin.com a mobile web browser. The app was nothing nore than a shortcut to for your phones already installed internet browser.
    
    Version 2.0 is a fully independent mobile application that not only simplifies the process of using your phone to access Punky Penguin, it adds extra fuctionality that is not available through a website & improves the users experience.
    
    By accessing your phones features it makes it easier to pesonalise your products. Add photos from the camera, gallery, social media or other apps. Assign contacts to date speciffic occasions like birthdays or anniversarys and your gift will be mailed so you don't need to remember.
    
    Subscribe to your favourite products & categories and you will be notified automatically about offers or latest news.
    
    Create acount or checkout in just a few clicks by signing up with current login details from Google, Facebook or Twitter.
    
    Lots more features included, full documentaion is available in the Play Store or on our website https://punkypenguin.com/punky-app-v2

---
+++ src/providers/service/config.ts
@@ -0,0 +1,98 @@
+import { Injectable } from '@angular/core';
+import { URLSearchParams } from '@angular/http';
+import { Headers } from '@angular/http';
+
+declare var oauthSignature: any;
+var headers = new Headers();
+headers.append('Content-Type', 'application/x-www-form-urlencoded; charset=UTF-8');
+
+@Injectable()
+export class Config {
+    
+    url: any = 'https://punkypenguin.com';
+    consumerKey: any = 'ck_b8ee03cddd6ad70543ffb0a5fef4740612715484';
+    consumerSecret: any = 'cs_7f57331032653f0d46da1140abc29c0ddc3f53d0';
+    oneSignalAppId: any = 'c002f531-0558-401f-8f9a-f187dc7c34f4';
+    googleProjectId: any = '588119324500';
+  webClientId: any = '588119324500-jua08o44mbtlrrvlvtki0i175jkviq65.apps.googleusercontent.com';  language: any = 'english';
+    appDir: any = 'ltr';
+
+
+    oauth: any;
+    signedUrl: any;
+    randomString: any;
+    oauth_nonce: any;
+    oauth_signature_method: any;
+    encodedSignature: any;
+    searchParams: any;
+    customer_id: any;
+    params: any;
+    options: any = {};
+    constructor() {
+
+        this.url = 'http://130.211.141.170/wordpress-demo';
+        this.consumerKey = 'ck_bf87b818dfd31d3a05257b4766bcd82cb5f3b07e';
+        this.consumerSecret = 'cs_11f6613b7eeeff10e797d34ac814058b67cda9c5';
+
+        this.options.withCredentials = true;
+        this.options.headers = headers;
+        this.oauth = oauthSignature;
+        this.oauth_signature_method = 'HMAC-SHA1';
+        this.searchParams = new URLSearchParams();
+        this.params = {};
+        this.params.oauth_consumer_key = this.consumerKey;
+        this.params.oauth_signature_method = 'HMAC-SHA1';
+        this.params.oauth_version = '1.0';
+    }
+    setOauthNonce(length, chars) {
+        var result = '';
+        for (var i = length; i > 0; --i) result += chars[Math.round(Math.random() * (chars.length - 1))];
+        return result;
+    }
+    setUrl(method, endpoint, filter) {
+        var key;
+        var unordered = {};
+        var ordered = {};
+        if (this.url.indexOf('https') >= 0) {
+            unordered = {};
+            if (filter) {
+                for (key in filter) {
+                    unordered[key] = filter[key];
+                }
+            }
+            unordered['consumer_key'] = this.consumerKey;
+            unordered['consumer_secret'] = this.consumerSecret;
+            Object.keys(unordered).sort().forEach(function(key) {
+                ordered[key] = unordered[key];
+            });
+            this.searchParams = new URLSearchParams();
+            for (key in ordered) {
+                this.searchParams.set(key, ordered[key]);
+            }
+            return this.url + endpoint + this.searchParams.toString();
+        }
+        else {
+            var url = this.url + endpoint;
+            this.params['oauth_consumer_key'] = this.consumerKey;
+            this.params['oauth_nonce'] = this.setOauthNonce(32, '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ');
+            this.params['oauth_timestamp'] = new Date().getTime() / 1000;
+            for (key in this.params) {
+                unordered[key] = this.params[key];
+            }
+            if (filter) {
+                for (key in filter) {
+                    unordered[key] = filter[key];
+                }
+            }
+            Object.keys(unordered).sort().forEach(function(key) {
+                ordered[key] = unordered[key];
+            });
+            this.searchParams = new URLSearchParams();
+            for (key in ordered) {
+                this.searchParams.set(key, ordered[key]);
+            }
+            this.encodedSignature = this.oauth.generate(method, url, this.searchParams.toString(), this.consumerSecret);
+            return this.url + endpoint + this.searchParams.toString() + '&oauth_signature=' + this.encodedSignature;
+        }
+    }
+}
